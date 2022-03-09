5311a1c30e6f6a8d822cdfd536b1e27a214990cf
Denis <ddd183@mail.ru>
2022-03-09 16:40:15 +0300

    function compInt, class work

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <QCoreApplication>
 
+int compare(int a, int b) {
+    return ((a & 0x00FF0000) - (b & 0x00FF0000)) > 0 ? 1 :
+           ((a & 0x00FF0000) - (b & 0x00FF0000)) < 0 ? -1 : 0;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+
+
 
     return a.exec();
 }
